0d1b597361d49f0402f247a5fc25264c33a090b1
BiaMara <79698634+BiaMara@users.noreply.github.com>
2021-03-01 16:58:59 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/biamara/git/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.biamara.git;
+
+/**
+ * @author Bianca Farias
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
